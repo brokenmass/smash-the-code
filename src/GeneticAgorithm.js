d05007c46f0f22d4992dc1d7a6b35ef0f8d16a99
Marco Massarotto <massa84@gmail.com>
2016-05-02 11:23:02 +0100

    [MM] genetic algorith v2

--- src/GeneticAgorithm.js
+++ src/GeneticAgorithm.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var selectors = require('./selectors');
 var utils = require('./utils');
 
 var DEFAULTS = {
@@ -10,29 +11,51 @@ var DEFAULTS = {
     return index;
   },
 
+  // At every generation the <elitarism> best individuals are guaranteed to
+  // be propagated to the next generation untouched
   elitarism: 2,
 
+  // At every generation (1 - <crossoverRate>) * <mutationRate> individuals
+  // will mutate using <mutationFunction> and the mutation will be added
+  // to the next generation
   mutationRate: 0.1,
   mutationFunction: function (phenotype) {
     return phenotype;
   },
 
+  // At every generation <crossoverRate> individuals will breed and generate
+  // new individual using the <crossoverFunction> to mix their phenotypes
+  // and the new indivuals will be added to the next generation.
   crossoverRate: 0.6,
   crossoverFunction: function (phenotypeA, phenotypeB) {
     return [phenotypeA, phenotypeB];
   },
 
+  // Calculate the fitness score of a phenotype
   fitnessFunction: function (phenotype) {
     return phenotype || 0;
   },
 
+  // After every generation it's possible to kill the unfitted individual
+  // in this way their phenotype will not be propagated to the next generation
+  // return false to 'kill' an individual
+  surviveFunction: function (phenotype, fitness) {
+    return true;
+  },
+
+  // How the algorithm should compare (sort) the individual fitness
   comparisonFunction: function (fitnessA, fitnessB) {
     return fitnessA > fitnessB ? -1 : 1;
   },
 
+  // Early termination of the algorithm based on specific condition
+  // return true to terminate
   terminationFunction: function (generation, population, stats) {
     return false;
-  }
+  },
+
+  select1: selectors.select1.tournament3,
+  select2: selectors.select2.randomLinearRank
 };
 
 function GeneticAlgorithm(options) {
@@ -54,25 +77,41 @@ function GeneticAlgorithm(options) {
 }
 
 GeneticAlgorithm.prototype.start = function start() {
-  var self = this;
-  var next = true;
-  for (var generation = 0; generation < this._generations && next; ++generation) {
+  var _this = this;
+  var generation, population, stats;
+  for (generation = 0; generation < this._generations; ++generation) {
     // reset for each generation
     this._internalState = {};
 
     // score and sort
-    var population = this.entities
+    var totalFitness = 0;
+    population = this.entities
       .map(function (entity) {
+        var fitness = _this.fitness(entity);
+        totalFitness += fitness;
         return {
-          fitness: self.fitness(entity),
+          fitness: fitness,
           entity: entity
         };
       })
       .sort(this._comparisonFunction);
 
-    var stats = {
-      'maximum': pop[0].fitness,
-      'minimum': pop[pop.length-1].fitness
+    var mean = totalFitness / population.length;
+    var stDev = Math.sqrt(population
+      .map(function (entity) {
+        return (entity.fitness - mean) * (entity.fitness - mean);
+      })
+      .reduce(function (a, b) { return a + b; }) / population.length);
+
+    population = population.filter(this._surviveFunction);
+
+    var deaths = this._populationSize - population.length;
+    stats = {
+      maximum: population[0].fitness,
+      minimum: population[population.length - 1].fitness,
+      mean: mean,
+      stDev: stDev,
+      deaths: deaths
     };
 
     if (this.terminationFunction(generation, population, stats)) {
@@ -81,89 +120,38 @@ GeneticAlgorithm.prototype.start = function start() {
 
     var i = 0;
     var newEntities = new Array(this._populationSize);
-    while(i < this._elitarism) {
+    while (i < this._elitarism) {
       newEntities[i] = population[i].entity;
       i++;
     }
-    while(i < this._populationSize - 1) {
+
+    while (i < this._populationSize - 1) {
       if (
         (Math.random() < this._crossoverRate) &&
-        ((i + 2) < (this.populationSize - 1))
+        ((i + 1) < (this.populationSize - 1))
       ) {
-        var childs = this.select2(this, population);
+        var parents = this._select2(this, population);
+        var childs = this._crossoverFunction(parents[0], parents[1]);
         newEntities[i] = childs[0];
-        newEntities[i+1] = childs[1];
+        newEntities[i + 1] = childs[1];
         i += 2;
       } else {
-        var entity = this.select1(this, population);
+        var entity = this._select1(this, population);
         if (Math.random() < this._mutationRate) {
-          entity = this._mut
+          entity = this._mutationFunction(utils.cloneJSON(entity));
         }
+
+        newEntities[i] = entity;
+        i++;
       }
-    }
-
-  }
-
-};
-
-
-
-
-
-  function randomizePopulationOrder( ) {
-
-    for( var index = 0 ; index < settings.population.length ; index++ ) {
-      var otherIndex = Math.floor( Math.random() * settings.population.length )
-      var temp = settings.population[otherIndex]
-      settings.population[otherIndex] = settings.population[index]
-      settings.population[index] = temp
     }
   }
 
   return {
-    evolve : function (options) {
-
-      if ( options ) {
-        settings = settingWithDefaults(options,settings)
-      }
-
-      populate()
-      randomizePopulationOrder()
-      compete()
-      return this
-    },
-    best : function() {
-      var scored = this.scoredPopulation()
-      var result = scored.reduce(function(a,b){
-        return a.score >= b.score ? a : b
-      },scored[0]).phenotype
-      return cloneJSON(result)
-    },
-    bestScore : function() {
-      return settings.fitnessFunction( this.best() )
-    },
-    population : function() {
-      return cloneJSON( this.config().population )
-    },
-    scoredPopulation : function() {
-      return this.population().map(function(phenotype) {
-        return {
-          phenotype : cloneJSON( phenotype ),
-          score : settings.fitnessFunction( phenotype )
-        }
-      })
-    },
-    config : function() {
-      return cloneJSON( settings )
-    },
-    clone : function(options) {
-      return geneticAlgorithmConstructor(
-        settingWithDefaults(options,
-          settingWithDefaults( this.config(), settings )
-          )
-        )
-    }
-  }
-}
+    generation: generation,
+    population: population,
+    stats: stats
+  };
+};
 
 module.exports = GeneticAlgorithm;
